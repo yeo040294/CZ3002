77210e74cc2950df6126de99cd1029bc2d27c946
hung <hungdp1999@gmail.com>
2021-09-14 17:46:38 +0800

    Add login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,30 +5,90 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { fetchPosts } from '../Redux/Actions/TwitterAction'
 
-
 class Home extends Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
-    render() {
-        return (
-            <div>
-                <MDBContainer>
-                    <MDBRow>
-                        {this.props.twitter && this.props.twitter.map(x => {
-                            return (
-                                <MDBCol lg="4">
-                                    <Card post={x} />
-                                </MDBCol>
-                            )
-                        })}
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         username: '',
+         passwprd: ''
+      }
+    }
 
-                    </MDBRow>
-                </MDBContainer>
-            </div>
+    handleUsernameChange = (event) => {
+      this.setState({
+        username: event.target.value
+      })
+    }
+    handlePasswordChange = (event) => {
+      this.setState({
+        password: event.target.value
+      })
+    }
+    handleSubmit = event => {
+      console.log(this.state.username)
+      console.log(this.state.password)
+      event.preventDefault()
+    }
+    render() {
+      const {username, password} = this.state
+        return (
+          <MDBContainer>    
+            <h2>Flip flop mental rotational</h2>             
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={this.handleUsernameChange}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="text"
+                className="form-control"
+                value={password}
+                onChange={this.handlePasswordChange}
+              />
+              <button type="submit">Submit</button> 
+            </form>
+          </MDBContainer>
         )
     }
 }
+
+
+// const InputPage = () => {
+//   return (
+//     <div className="form-group">
+//       <label htmlFor="username">Username</label>
+//       <input
+//         type="text"
+//         className="form-control"
+//         value={this.state.username}
+//         onChange={this.handleUsernameChange}
+//       />
+//       <label htmlFor="password">Password</label>
+//       <input
+//         type="text"
+//         className="form-control"
+//         id="password"
+//       />
+//     <label htmlFor="userType">User type</label>
+//       <div>
+//         <select className="browser-default custom-select">
+//           <option value="1">Patient</option>
+//           <option value="2">Medical Staff</option>
+//           <option value="3">Administrator</option>
+//         </select>
+//       </div>
+//     </div>
+//   );
+// }
+
 Home.propTypes = {
     fetchPosts: PropTypes.func.isRequired
 }
